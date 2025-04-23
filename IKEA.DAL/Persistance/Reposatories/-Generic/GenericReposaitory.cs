6070a3f9628339d2e9cdff5377822de39346a622
Khaled_Mostafa99 <khaledmostafa0033@gmail.com>
2025-04-23 12:28:57 +0200

    04 generic reposaitiory

---
+++ IKEA.DAL/Persistance/Reposatories/-Generic/GenericReposaitory.cs
@@ -0,0 +1,66 @@
+﻿using IKEA.DAL.Models;
+using IKEA.DAL.Models.Employees;
+using IKEA.DAL.Persistance.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.DAL.Persistance.Reposatories._Generic
+{
+    public class GenericReposaitory<T> : IGenericReposatiory<T> where T : ModelBase
+    {
+
+        private readonly ApplicationDbContext dbContext;
+        public GenericReposaitory(ApplicationDbContext context)
+        {
+            dbContext = context;
+
+        }
+
+        public IEnumerable<T> GetAll(bool WithNoTracking = true)
+        {
+            if (WithNoTracking)
+                return dbContext.Set<T>().Where(D => D.IsDeleted == false).AsNoTracking().ToList();
+
+            return dbContext.Set<T>().Where(D => D.IsDeleted == false).ToList();
+        }
+
+
+        public T? GetById(int id)
+        {
+            var item = dbContext.Set<T>().Find(id);
+            //var Department = dbContext.Departments.Local.FirstOrDefault(D => D.Id == id);
+
+            //if(Department == null)
+            //    Department=dbContext.Departments.FirstOrDefault(D => D.Id == id);
+
+            return item;
+        }
+
+
+        public int Add(T item)
+        {
+            dbContext.Set<T>().Add(item);
+            return dbContext.SaveChanges();
+        }
+        public int Update(T item)
+        {
+            dbContext.Set<T>().Update(item);
+            return dbContext.SaveChanges();
+        }
+
+
+        public int Delete(T item)
+        {
+            item.IsDeleted = true;
+            dbContext.Set<T>().Update(item);
+
+            return dbContext.SaveChanges();
+        }
+
+       
+    }
+}  
---
+++ IKEA.DAL/Persistance/Reposatories/-Generic/IGenericReposatiory.cs
@@ -0,0 +1,24 @@
+﻿using IKEA.DAL.Models;
+using IKEA.DAL.Models.Departments;
+using IKEA.DAL.Models.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.DAL.Persistance.Reposatories._Generic
+{
+    public interface IGenericReposatiory<T>where T:ModelBase
+    {
+        IEnumerable<T> GetAll(bool WithNoTracking = true);
+
+        T GetById(int id);
+
+        int Add(T Item);
+        int Delete(T Item);
+        int Update(T Item);
+
+      
+    }
+}
